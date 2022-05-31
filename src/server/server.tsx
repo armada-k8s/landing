1939b2a76a6fb5dbdf8e1086c4f2270ef19db433
Em <epithe@gmail.com>
2022-05-31 11:45:27 -0400

    added a procfile

--- src/server/server.tsx
+++ src/server/server.tsx
@@ -36,4 +36,4 @@ app.use(
   }
 );
 
-app.listen(3000, () => console.log('server is listening on port 3000'));
+app.listen(process.env.PORT || 3000, () => console.log('server is listening on port 3000'));
